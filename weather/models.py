1b7e04fd265ed970a0a0214e319568a5aae5f533
FMaslina <FMaslina@yandex.ru>
2024-11-04 16:20:21 +0700

    Add CityWeather model

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from core.models.base import TimeStampModel
+
+
+class CityWeather(TimeStampModel):
+    city_name = models.CharField(max_length=64, verbose_name="Название города")
+    temperature = models.FloatField(verbose_name="Температура в городе")
+    atmospheric_pressure = models.IntegerField(verbose_name="Атмосферное давление")
+    wind_speed = models.FloatField(verbose_name="Скорость ветра")
+
+    class Meta:
+        verbose_name = "Погода по городу"
+        verbose_name_plural = "Погода по городам"
