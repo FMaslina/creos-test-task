9d796ab9f1005b006a5dccf2d6d8652450126df7
FMaslina <FMaslina@yandex.ru>
2024-11-05 15:50:53 +0700

    Add WeatherRequestHistorySerializer

--- weather/serializers.py
+++ weather/serializers.py
@@ -1,9 +1,17 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, CharField
 
-from weather.models import CityWeather
+from weather.models import CityWeather, WeatherRequestsHistory
 
 
 class CityWeatherSerializer(ModelSerializer):
     class Meta:
         model = CityWeather
         fields = "__all__"
+
+
+class WeatherRequestHistorySerializer(ModelSerializer):
+    city_name = CharField(source='city.city_name')
+
+    class Meta:
+        model = WeatherRequestsHistory
+        fields = ['id', 'city_name', 'request_type', 'created_at']
