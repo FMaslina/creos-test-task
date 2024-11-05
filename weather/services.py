6629ee1f6b4dc1a67c2136ce5df635dd982d43c2
FMaslina <FMaslina@yandex.ru>
2024-11-05 15:16:59 +0700

    Add WeatherService

---
+++ weather/services.py
@@ -0,0 +1,69 @@
+import os
+from datetime import timedelta
+
+import requests
+from django.utils import timezone
+from dotenv import load_dotenv
+
+from weather.exceptions import WeatherServiceError
+from weather.models import CityWeather, WeatherRequestsHistory
+from weather.serializers import CityWeatherSerializer
+
+load_dotenv()
+
+
+class WeatherService:
+    def __init__(self):
+        self.weather_api_key = os.getenv("YANDEX_WEATHER_API_KEY")
+        self.geocode_api_key = os.getenv("YANDEX_GEOCODE_API_KEY")
+        self.serializer = CityWeatherSerializer
+
+    def get_city_coordinates(self, city: str) -> dict:
+        geocode_url = f"https://geocode-maps.yandex.ru/1.x?apikey={self.geocode_api_key}&geocode={city}&format=json"
+        response = requests.get(geocode_url).json()
+        pos = response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"]["pos"]
+        longitude, latitude = pos.split(" ")
+
+        coordinates = dict(longitude=longitude, latitude=latitude)
+
+        return coordinates
+
+    def get_weather(self, city: str, request_type: str = 'web') -> dict:
+        city_weather_obj = CityWeather.objects.filter(city_name=city).first()
+        time_threshold = timezone.now() - timedelta(minutes=30)
+
+        if not city_weather_obj or city_weather_obj.updated_at < time_threshold:
+            city_coordinates = self.get_city_coordinates(city)
+
+            headers = {
+                'X-Yandex-Weather-Key': self.weather_api_key
+            }
+            url = (f'https://api.weather.yandex.ru/v2/forecast?'
+                   f'lat={city_coordinates["latitude"]}&'
+                   f'lon={city_coordinates["longitude"]}')
+
+            try:
+                response = requests.get(url, headers=headers)
+                response.raise_for_status()
+                weather_data = response.json()
+
+                weather_params = {
+                    'temperature': weather_data["fact"]["temp"],
+                    'atmospheric_pressure': weather_data["fact"]["pressure_mm"],
+                    'wind_speed': weather_data["fact"]["wind_speed"]
+                }
+
+                city_weather_obj, _ = CityWeather.objects.update_or_create(
+                    city_name=city,
+                    defaults=weather_params
+                )
+
+            except (KeyError, requests.RequestException):
+                raise WeatherServiceError("Не удалось получить данные о погоде в городе")
+
+        WeatherRequestsHistory.objects.create(
+            city=city_weather_obj,
+            request_type=request_type
+        )
+
+        return self.serializer(city_weather_obj).data
