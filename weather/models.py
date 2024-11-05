6943f9da97dc779cf3a59aa5253b1a8437f5d8df
FMaslina <FMaslina@yandex.ru>
2024-11-05 15:16:40 +0700

    Add WeatherRequestHistory model

--- weather/models.py
+++ weather/models.py
@@ -4,7 +4,7 @@ from core.models.base import TimeStampModel
 
 
 class CityWeather(TimeStampModel):
-    city_name = models.CharField(max_length=64, verbose_name="Название города")
+    city_name = models.CharField(max_length=64, verbose_name="Название города", unique=True)
     temperature = models.FloatField(verbose_name="Температура в городе")
     atmospheric_pressure = models.IntegerField(verbose_name="Атмосферное давление")
     wind_speed = models.FloatField(verbose_name="Скорость ветра")
@@ -12,3 +12,12 @@ class CityWeather(TimeStampModel):
     class Meta:
         verbose_name = "Погода по городу"
         verbose_name_plural = "Погода по городам"
+
+
+class WeatherRequestsHistory(TimeStampModel):
+    city = models.ForeignKey(CityWeather, on_delete=models.CASCADE)
+    request_type = models.CharField(
+        max_length=20,
+        choices=[('web', 'Web'), ('telegram', 'Telegram')],
+        default='web'
+    )
