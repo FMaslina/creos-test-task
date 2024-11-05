037637025ef0fec223222b2a08fedb2f17a02ddf
FMaslina <FMaslina@yandex.ru>
2024-11-05 15:52:15 +0700

    Add requests endpoint

--- weather/urls.py
+++ weather/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from weather.views import GetWeather
+from weather.views import GetWeather, WeatherRequestsHistoryView
 
 urlpatterns = [
-    path('weather', GetWeather.as_view())
+    path('weather', GetWeather.as_view()),
+    path('requests/', WeatherRequestsHistoryView.as_view())
 ]
